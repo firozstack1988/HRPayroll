097c158edf9799ed416a454b5da6578ebba08902
firozstack1988 <152399106+firozstack1988@users.noreply.github.com>
2023-12-18 14:19:27 +0600

    Add files via upload

---
+++ HolidayService.java
@@ -0,0 +1,8 @@
+package com.firoztechi.UserApp.UserApp.Service;
+
+import com.firoztechi.UserApp.UserApp.Entity.Holidays;
+
+public interface HolidayService {
+
+	public void add(Holidays holidays)throws Exception;
+}
---
+++ SalaryProcessService.java
@@ -0,0 +1,8 @@
+package com.firoztechi.UserApp.UserApp.Service;
+
+import com.firoztechi.UserApp.UserApp.Model.SalaryProcess;
+
+public interface SalaryProcessService {
+
+	public void add(SalaryProcess salaryProcess)throws Exception;
+}
