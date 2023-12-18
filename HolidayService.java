77591d00da06c683efd4a41547b5c60d3cc30887
firozstack1988 <152399106+firozstack1988@users.noreply.github.com>
2023-12-18 14:27:19 +0600

    Delete HolidayService.java

--- HolidayService.java
+++
@@ -1,8 +0,0 @@
-package com.firoztechi.UserApp.UserApp.Service;
-
-import com.firoztechi.UserApp.UserApp.Entity.Holidays;
-
-public interface HolidayService {
-
-	public void add(Holidays holidays)throws Exception;
-}
