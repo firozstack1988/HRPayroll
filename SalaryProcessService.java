37d314da9631abdc1e60cee3eadaaaf7e2b2404e
firozstack1988 <152399106+firozstack1988@users.noreply.github.com>
2023-12-18 14:29:58 +0600

    Delete SalaryProcessService.java

--- SalaryProcessService.java
+++
@@ -1,8 +0,0 @@
-package com.firoztechi.UserApp.UserApp.Service;
-
-import com.firoztechi.UserApp.UserApp.Model.SalaryProcess;
-
-public interface SalaryProcessService {
-
-	public void add(SalaryProcess salaryProcess)throws Exception;
-}
