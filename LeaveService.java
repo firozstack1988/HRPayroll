d36f3aa6a59c70ce3076baa8a64d1ddb32971db9
firozstack1988 <152399106+firozstack1988@users.noreply.github.com>
2023-12-18 14:28:46 +0600

    Delete LeaveService.java

--- LeaveService.java
+++
@@ -1,9 +0,0 @@
-package com.firoztechi.UserApp.UserApp.Service;
-
-import com.firoztechi.UserApp.UserApp.Entity.LeaveEntry;
-
-public interface LeaveService {
-
-	public void add(LeaveEntry leave) throws Exception;
-	
-}
