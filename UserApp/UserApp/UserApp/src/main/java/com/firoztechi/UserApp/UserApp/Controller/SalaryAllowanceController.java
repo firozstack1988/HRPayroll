4acd68613298b9001f64e945f33984a1215efcf3
firozstack1988 <152399106+firozstack1988@users.noreply.github.com>
2024-02-29 11:15:51 +0600

    Add files via upload

--- UserApp/UserApp/UserApp/src/main/java/com/firoztechi/UserApp/UserApp/Controller/SalaryAllowanceController.java
+++ UserApp/UserApp/UserApp/src/main/java/com/firoztechi/UserApp/UserApp/Controller/SalaryAllowanceController.java
@@ -2,16 +2,21 @@ package com.firoztechi.UserApp.UserApp.Controller;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.firoztechi.UserApp.UserApp.Entity.SalaryAllowance;
+import com.firoztechi.UserApp.UserApp.Entity.Users;
+import com.firoztechi.UserApp.UserApp.Model.EmployeeModel;
 import com.firoztechi.UserApp.UserApp.Model.ResponseModel;
 import com.firoztechi.UserApp.UserApp.Service.SalaryAllowanceService;
 import com.firoztechi.UserApp.UserApp.Utility.AppUtility;
@@ -35,6 +40,35 @@ public class SalaryAllowanceController {
 				salaryAllowance.setYear(new Date().getYear());
 				salaryAllowanceService.add(salaryAllowance);
 				response.setSuccess(AppUtility.SUCCESS_MESSAGE);
+		}
+		catch(Exception e) {
+			response.setErrorMsg(AppUtility.FAILURE_MSG);
+			throw new Exception(e.getLocalizedMessage());
+		}
+	return response;
+	}
+	
+	@GetMapping("/allowanceList")
+	public ResponseModel allowanceList()throws Exception {
+		ResponseModel response=new ResponseModel();
+		try {
+			List<SalaryAllowance> allowanceList=salaryAllowanceService.getAllowanceList();
+				 response.setSuccess(AppUtility.SUCCESS_MESSAGE);
+				 response.setContent(allowanceList);
+		}
+		catch(Exception e) {
+			response.setErrorMsg(AppUtility.FAILURE_MSG);
+			throw new Exception(e.getLocalizedMessage());
+		}
+	return response;
+	}
+	@GetMapping("/allowanceById/{id}")
+	public ResponseModel employeeById(@PathVariable long id)throws Exception {
+		ResponseModel response=new ResponseModel();
+		try {
+		SalaryAllowance allowance=salaryAllowanceService.getAllowance(id);
+				 response.setSuccess(AppUtility.SUCCESS_MESSAGE);
+				 response.setContent(allowance);
 		}
 		catch(Exception e) {
 			response.setErrorMsg(AppUtility.FAILURE_MSG);
